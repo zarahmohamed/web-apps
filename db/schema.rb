31baf42768712bc5991d820639341dc2526410e1
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-05-05 19:29:10 +0000

    i love tacos

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_225938) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_225938) do
   create_table "activities", force: :cascade do |t|
     t.integer "salesperson_id"
     t.integer "contact_id"
     t.string "note"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "city"
     t.string "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "contacts", force: :cascade do |t|
@@ -34,16 +33,16 @@ ActiveRecord::Schema.define(version: 2022_05_04_225938) do
     t.string "email"
     t.string "phone_number"
     t.integer "company_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "salespeople", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
